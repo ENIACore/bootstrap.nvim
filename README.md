f25c6c63b5dca1b80d5f6dcdc77711ffa9a66c4b
Chase Lamkin <CLamkin@wsgc.com>
2026-06-02 09:55:38 -0700

    Added jshint linter

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 - ensure java homes in `/Library/Java/JavaVirtualMachines/*` via `sudo ln -sfn /opt/homebrew/opt/openjdk@<version>/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-<version>.jdk`
 - automate python (3.9), npm (latest), golang installations
 - automate `:Copilot setup` command and ensure user checks yes on `Copilot chat in the IDE enabled in GitHub settings`
+- automate jshint install via `npm -g install jshint`
 
 # TODO
 
--- after/plugin/nvim-lint.lua
+++ after/plugin/nvim-lint.lua
@@ -10,9 +10,10 @@ registry:install_pkg_list(linters)
 local lint = require("lint")
 lint.linters_by_ft = {
 	java = { "checkstyle" },
+	javascript = { "jshint" }, -- jshint installed manually via `npm install -g jshint`
 }
 
-require("lint.linters.checkstyle").config_file = vim.fn.expand("~/.config/nvim/lint/java_checkstyle.xml")
+require("lint.linters.checkstyle").config_file = vim.fn.expand("~/.config/nvim/lint/java-checkstyle.xml")
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
