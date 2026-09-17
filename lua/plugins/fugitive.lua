683122e3fe5b50953fe74e83487725af430f39df
Chase Lamkin <CLamkin@wsgc.com>
2026-09-17 15:16:58 -0700

    Added easy get ours and get theirs vim fugitive bindings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,14 @@
 return {
 	"tpope/vim-fugitive",
+	config = function()
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			pattern = "*",
+			callback = function()
+				if vim.wo.diff then
+					vim.keymap.set("n", "<leader>gh", ":diffget //2<CR>", { buffer = true, silent = true })
+					vim.keymap.set("n", "<leader>gl", ":diffget //3<CR>", { buffer = true, silent = true })
+				end
+			end,
+		})
+	end,
 }
